e3cd930aeaa89b9e587158eaf5b1acde569de0f8
anupghimire12 <ghimireanup12@gmail.com>
2025-08-19 16:35:16 +0545

    Remove exposed Google Maps API key for security

--- public/admin/assets/js/google-charts.js
+++ public/admin/assets/js/google-charts.js
@@ -1,10 +1,9 @@
 // Region Charts Starts
 
 google.charts.load('current', {
-  'packages': ['geochart'],
-  // Note: you will need to get a mapsApiKey for your project.
+  'packages': ['geochart']
+  // Note: mapsApiKey removed for security. Use environment variables or secure config for API keys.
   // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
 });
 google.charts.setOnLoadCallback(drawRegionsMap);
 
